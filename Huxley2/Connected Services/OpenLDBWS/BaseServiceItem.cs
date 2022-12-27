30fc6887a125fe93f1f033c10c0deefbe15fa8e0
Edward Chung <edward@edward-designer.com>
2022-12-27 14:23:05 +0000

    Update BaseServiceItem.cs

--- Huxley2/Connected Services/OpenLDBWS/BaseServiceItem.cs
+++ Huxley2/Connected Services/OpenLDBWS/BaseServiceItem.cs
@@ -14,6 +14,6 @@ namespace OpenLDBWS
         public Guid ServiceIdGuid => new Guid();
 
         [SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "Not a URL")]
-        public string ServiceIdUrlSafe => WebEncoders.Base64UrlEncode(Convert.FromBase64String(serviceIDField.PadRight(s.Length + (s.Length * 3) % 4, '=')));
+        public string ServiceIdUrlSafe => WebEncoders.Base64UrlEncode(Convert.FromBase64String(serviceIDField.PadRight(serviceIDField.Length + (serviceIDField.Length * 3) % 4, '=')));
     }
 }
